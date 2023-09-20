351c77e13fadeea9075baf112f22aabb3df5e8fd
Flasky06 <bonnienjuguna106@gmail.com>
2023-09-20 13:06:18 +0300

    woked on buttons

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+	<button @click="handleButtonClick" :style="{ backgroundColor: customized }">
+		{{ name }}
+	</button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const { name, customized } = defineProps(["name", "customized"]);
+const emit = defineEmits(["onIncrease", "onDecrease"]);
+
+const handleButtonClick = () => {
+	emit("onIncrease", 2);
+	emit("onDecrease", 2);
+};
+</script>
+
+<!-- <style lang="scss" scoped>
+/* Your scoped CSS styles go here */
+</style> -->
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="text-red-800">Viewing the homeview</div>
+	<Button
+		class="mx-2 p-4 w-24 text-white"
+		name="add"
+		@onIncrease="inc"
+		customized="red"
+	/>
+	<span>{{ count }}</span>
+
+	<Button
+		class="mx-2 p-4 w-24 text-white"
+		name="minus"
+		@onDecrease="dec"
+		customized="green"
+	/>
+	<HelloWorld />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Button from "../components/Button.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const count = ref(0);
+const inc = (n) => {
+	count.value += n;
+};
+const dec = (n) => {
+	count.value -= n;
+};
+</script>
+
+<style lang="scss" scoped></style>
